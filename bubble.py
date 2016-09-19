a38c34f931a0fbc63dc91d2d4156405fb961108d
Rachel Kelly <rkellyalso@gmail.com>
2016-09-19 16:34:24 -0700

    Getting a list index error as expected, but I think I can     conditional that out without much trouble.  Now to figure out     how to get it to go over and over and over it until True :)

--- bubble.py
+++ bubble.py
@@ -6,30 +6,26 @@ import itertools
 
 def bubble(in_string):
 
+    lo = []
     for i in in_string:
         lo.append(int(i))
     j = 0
-    while j < len(lo):
-        k = lo[j+1]
-        if k == lo[-1]: 
-            if lo[j] > lo[k]: # or should this be `if lo[j] > k`
-                intermed = lo[k] # set smaller value at EOL to intermed
-                lo[k] = lo[j] # move value at lo[j] to EOL bc is bigger
-                lo[j] = intermed # set lower value in list to smaller value
-            break
-        if lo[j] > lo[j+1]:
-            intermediary = lo[j+1]
-        j += 1
 
-    print "unsorted %s" % lo
-    for j in lo:
-        print j
-        if j == lo[-1]: # this is a check to see if we're at the end of the list
-            return lo
-        if lo[j] <= lo[j+1]:
-            pass
-        elif lo[j] > lo[j+1]:
-            lo[j+1] = lo[j]
+    while j < len(lo):
+        if lo[j] > lo[j+1]:
+            smaller = lo[j+1]
+            bigger = lo[j]
+            lo[j] = smaller
+            lo[j+1] = bigger # need to conditional this one out
+        j += 1
+        print lo
+
+#        if j == lo[-1]: # this is a check to see if we're at the end of the list
+#            return lo
+#        if lo[j] <= lo[j+1]:
+#            pass
+#        elif lo[j] > lo[j+1]:
+#            lo[j+1] = lo[j]
 
 bubble('63782541')
 
