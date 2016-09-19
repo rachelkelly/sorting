29e3abfb7289a29a8ff9500ca43021301793f713
Rachel Kelly <rkellyalso@gmail.com>
2016-09-19 13:00:08 -0700

    making progress - denoting the +1 from index point,     judging that against last item in list rather than     the index itself.  This should prevent "segfaulting"     (which of course doesn't happen in this way in Python).

--- bubble.py
+++ bubble.py
@@ -1,28 +1,35 @@
-# Bubble Sort compares only two items at a time, then looks at the entirety
-# and then compares them one by one again.
+# Bubble Sort compares only two items at a time, then confirms by comparing
+# them one by one again.  Takes as long as it takes.  Is O(n^2) complexity
 
 import unittest
 import itertools
 
 def bubble(in_string):
 
-
-    while i < len(lo):
-        itertools.cycle(lo)
-        i += 1
-
-    listy = []
     for i in in_string:
-        listy.append(int(i))
-    print "unsorted %s" % listy
-    for j in listy:
+        lo.append(int(i))
+    j = 0
+    while j < len(lo):
+        k = lo[j+1]
+        if k == lo[-1]: 
+            if lo[j] > lo[k]: # or should this be `if lo[j] > k`
+                intermed = lo[k] # set smaller value at EOL to intermed
+                lo[k] = lo[j] # move value at lo[j] to EOL bc is bigger
+                lo[j] = intermed # set lower value in list to smaller value
+            break
+        if lo[j] > lo[j+1]:
+            intermediary = lo[j+1]
+        j += 1
+
+    print "unsorted %s" % lo
+    for j in lo:
         print j
-        if j == listy[-1]: # this is a check to see if we're at the end of the list
-            return listy
-        if listy[j] <= listy[j+1]:
+        if j == lo[-1]: # this is a check to see if we're at the end of the list
+            return lo
+        if lo[j] <= lo[j+1]:
             pass
-        elif listy[j] > listy[j+1]:
-            listy[j+1] = listy[j]
+        elif lo[j] > lo[j+1]:
+            lo[j+1] = lo[j]
 
 bubble('63782541')
 
