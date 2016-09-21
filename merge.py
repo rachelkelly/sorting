5198c14f04f35e06fdb247d246282e1652551067
Rachel Kelly <rkellyalso@gmail.com>
2016-09-20 23:27:37 -0700

    just getting started on merge sort

--- merge.py
+++ merge.py
@@ -3,4 +3,9 @@
 # and compares them by fours, then sorts and compares them by eights, 
 # and so forth.  Is complexity O(n n).  O(log n)?
 
+def merge(in_string):
+    lo = []
+    for i in in_string:
+        lo.append(int(i))
 
+    # now to collect them in groups of 2, putting the lower number first
