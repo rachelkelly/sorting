47f1fc63908edc79ab7eaa943746df1fdc79dde8
Rachel Kelly <rkellyalso@gmail.com>
2016-09-24 14:06:56 -0700

    making progress.  it is either out of index OR I lose the last item.  there is a lesson in this ...

--- merge.py
+++ merge.py
@@ -14,8 +14,11 @@ def merge(in_string):
     new = []
     while i < len(lo):
         while len(new) < 2:
+            print "index: %d" %i
             if i == (len(lo)-1):
-                new[0] = i
+                #new[0] = i
+                i += 1
+                break
             else:
                 new.append(lo[i])
                 if len(new) == 2: # now that new is full, we compare
@@ -24,10 +27,11 @@ def merge(in_string):
                         smaller = new[1]
                         new[0] = smaller
                         new[1] = bigger
-        #i += 1 
+            
+            i += 1 
         twos.append(new)
         new = []
-        i += 1
+        #i += 1
     print twos
 
 merge('36728541')
